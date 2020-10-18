d9575a46ed38af36f07ab2ec2263a769ff2075f5
maruti bhosale <marutibhosale31@gmail.com>
2020-10-18 12:14:47 +0530

    Add: part_time_wage

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -10,9 +10,10 @@ is_present=$((RANDOM%2))
 if [ $is_present -eq 1 ]
 then
     echo "Employee is present"
-else:
+else
    echo "Employee is not present"
 fi
+
 
 # employee daily wage (UC2)
 
@@ -22,3 +23,13 @@ FULL_DAY_HOUR=8
 daily_wage=$(($WAGE_PER_HOUR*$FULL_DAY_HOUR))
 
 echo "Daily employee wage is: "$daily_wage
+
+
+
+# employee part time wage (UC3)
+
+PART_TIME_HOUR=4
+part_time_wage=$(($WAGE_PER_HOUR*$PART_TIME_HOUR))
+
+echo "Part time wage is : "$part_time_wage
+
