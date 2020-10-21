65dbf7190824c14ddaa2c0eb70dae2508ab6251b
maruti bhosale <marutibhosale31@gmail.com>
2020-10-21 23:57:07 +0530

    Add: email validation

--- pattern/user_registration.sh
+++ pattern/user_registration.sh
@@ -4,7 +4,10 @@ shopt -s extglob
 
 read -p "Enter first name : " fName
 
-read -p  "Enter last name :"lName
+read -p  "Enter last name :" lName
+
+read  -p "Enter last name :" email
+
 
 
 validateName()
@@ -14,9 +17,9 @@ validateName()
 
      if [[ $1 =~ $fNamePat ]]
      then
-        echo "yes"
+        echo "Name is valid"
      else
-        echo "no"
+        echo "Name is not valid"
     fi
 
 
@@ -24,3 +27,19 @@ validateName()
 
 validateName $fName
 validateName $lName
+
+validateEmail()
+{
+    emailPat="^([a-z]{1})+[a-zA-Z0-9]*[\.\_\+\-]?[a-zA-z0-9]+\@[a-z0-9]{1,5}\.((com|net)|\.(com|au))+$"
+     if [[ $1 =~ $emailPat ]]
+     then
+        echo "Email is valid"
+     else
+        echo "not valid email"
+    fi
+
+
+}
+
+
+validateEmail $email
